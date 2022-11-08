3911a321fb5a8833c50d42d72f0753a8e788a2a5
Masahiro Hiramori <mhg00g13@gmail.com>
2022-11-08 18:27:44 +0900

    add todo list view

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
-    return HttpResponse("<h1>Hello world</h1>")
+from django.views.generic import ListView
+
+from .models import Todo
+
+class TodoList(ListView):
+    model = Todo
+    context_object_name = "tasks"
