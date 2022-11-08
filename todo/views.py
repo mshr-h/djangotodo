cabc75ffab27d4ae2bf5d858ba6662182236b3fe
Masahiro Hiramori <mhg00g13@gmail.com>
2022-11-08 18:32:04 +0900

    add task detail

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Todo
 
 class TodoList(ListView):
     model = Todo
     context_object_name = "tasks"
+
+class TodoDetail(DetailView):
+    model = Todo
+    context_object_name = "task"
